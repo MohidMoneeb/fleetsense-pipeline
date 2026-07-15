f9e41dad42ed83e93ad555bdaf1a5b471aa232cd
Mohiud Moneeb <mm1204@duke.edu>
2026-07-15 07:17:54 +0500

    chore: scaffold repo structure and gitignore

---
+++ src/hello_lambda.py
@@ -0,0 +1,5 @@
+import json
+
+def lambda_handler(event, context):
+    print("Received event:", json.dumps(event))
+    return {"statusCode": 200, "body": json.dumps({"message": "hello from fleetsense CLI"})}
